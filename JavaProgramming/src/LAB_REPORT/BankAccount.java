f085a20642e58dbe88048b031c8e59e6e2af02a7
maRia-1506A <zannatulfarzana363@gmail.com>
2025-04-10 00:21:16 +0600

    java

--- JavaProgramming/src/LAB_REPORT/BankAccount.java
+++ JavaProgramming/src/LAB_REPORT/BankAccount.java
@@ -3,89 +3,118 @@
 import java.util.Scanner;
 
 public class BankAccount {
-    private String accountNumber;
-    private String accountHolderName;
+    private String accountHolderName, accountNumber;
     private double balance;
 
-    //setter accountNumber
-    public void setAccountNumber(String accountNumber) {
-        this.accountNumber= accountNumber;
-    }
-
-    //getter accountNumber
-    public String getAccountNumber() {
-        return accountNumber;
-    }
-
-    //setter accountHolderName
+    //set accountHolderName
     public void setAccountHolderName(String accountHolderName) {
         this.accountHolderName= accountHolderName;
     }
-
-    //getter accountHolderName
+    //get accountHolderName
     public String getAccountHolderName() {
         return accountHolderName;
     }
 
-    
-    //setter balance
+    //set accountNumber
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber= accountNumber;
+    }
+    //get accountNumber
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    //set balance
     public void setBalance(double balance) {
         this.balance= balance;
     }
-
-    //getter balance
+    //get balance
     public double getBalance() {
         return balance;
     }
 
-    //display method
+    //method deposit
+    public void deposit(double amount) {
+        if(amount > 0) {
+            amount += balance;
+            System.out.println("Deposit balance: "+amount);
+        } else {
+            System.out.println("Only positive deposit is allowed");
+        }
+    }
+
+    //method withdraw
+    public void withdraw(double amount) {
+        if(balance >= amount && amount > 0) {
+            amount -= balance;
+            System.out.println("Withdraws money: "+amount);
+        } else {
+            System.out.println("The balance is sufficient");
+        }
+    }
+
+    //method display
     public void display() {
-        System.out.println("Account Holder Number: "+getAccountNumber());
-        System.out.println("Account Holder Name: "+getAccountHolderName());
-        System.out.println("Balnce: "+getBalance());
+        System.out.println("Account Holder Name: "+accountHolderName);
+        System.out.println("Account Number: "+accountNumber);
+        System.out.println("Balance: "+balance);
     }
 
-    //deposite method
-    public void deposite(double ammount) {
-        if(ammount > 0) {
-            ammount += balance;
-            System.out.println("Deposite: "+ammount);
-        } else {
-            System.out.println("Deposite ammount must be positive\n");
-        }
-    }
-
-    //withdraw method
-    public void withdraw(double ammount) {
-        if(ammount > 0 && balance >= ammount) {
-                ammount -= balance;
-                System.out.println("Withdrawn: "+ammount);
-        } else {
-                System.out.println("Insuffecient balance\n");
-        }
-    }
-
-    public static void main(String[] args) {
-        BankAccount ba= new BankAccount();
+    //method user
+    public void user() {
         Scanner input= new Scanner(System.in);
 
-        System.out.println("Enter Account Holder Number: ");
-        ba.setAccountNumber(input.nextLine());
-        System.out.println("Enter Account Holder Name: ");
-        ba.setAccountHolderName(input.nextLine());
-        System.out.println("Enter balance: ");
-        ba.setBalance(input.nextDouble());
+        System.out.println("Enter account holder name: "); 
+        accountHolderName= input.nextLine();
+        System.out.println("Enter account number: "); 
+        accountNumber= input.nextLine();
+        System.out.println("Enter balance: "); 
+        balance= input.nextDouble();
 
-        ba.display();
+        System.out.println("Enter deposit amount: ");
+        double depositAmount= input.nextDouble();
+        deposit(depositAmount);
 
-        System.out.println("Enter the ammount you want to deposite: ");
-        double depositeAmmount= input.nextDouble();
-        ba.deposite(depositeAmmount);
-        ba.display();
+        System.out.println("Enter withdrawn ammount: ");
+        double withdrawAmount= input.nextDouble();
+        withdraw(withdrawAmount);
+    }
 
-        System.out.println("Enter the ammount you want to withdraw: ");
-        double withdrawAmmount= input.nextDouble();
-        ba.deposite(withdrawAmmount);
+}
+
+class Maain {
+    public static void main(String[] args) {
+        BankAccount ba= new BankAccount();
+        ba.user();
         ba.display();
     }
 }
+
+// import java.util.Scanner;
+
+// public class BankAccount {
+   
+//     public static void main(String[] args) {
+//         BankAccount ba= new BankAccount();
+//         Scanner input= new Scanner(System.in);
+
+//         System.out.println("Enter Account Holder Number: ");
+//         ba.setAccountNumber(input.nextLine());
+//         System.out.println("Enter Account Holder Name: ");
+//         ba.setAccountHolderName(input.nextLine());
+//         System.out.println("Enter balance: ");
+//         ba.setBalance(input.nextDouble());
+
+//         ba.display();
+
+//         System.out.println("Enter the ammount you want to deposite: ");
+//         double depositeAmmount= input.nextDouble();
+//         ba.deposite(depositeAmmount);
+//         ba.display();
+
+//         System.out.println("Enter the ammount you want to withdraw: ");
+//         double withdrawAmmount= input.nextDouble();
+//         ba.deposite(withdrawAmmount);
+//         ba.display();
+//     }
+// }
